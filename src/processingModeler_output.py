532c64ee8596d3eb3c16e81501de9a2678817254
Crghilardi <Crghilardi@yahoo.com>
2017-08-11 18:40:21 -0700

    remove uneeded files

--- src/processingModeler_output.py
+++
@@ -1,6 +0,0 @@
-##=name
-##output=point
-##vectorlayer=vector
-##id=optional fieldvectorlayer
-##numberofpoints=number1
-outputs_SCRIPTCRGGRIDWITHINPOLYGON_1=processing.runalg('script:crggridwithinpolygon', )
--- src/regular_grid_export.py
+++
@@ -1,2 +0,0 @@
-##=name
-outputs_QGISREGULARPOINTS_1=processing.runalg('qgis:regularpoints', ['0,1,1,0'],0.0001,0.0,False,False,None)
