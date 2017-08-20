653b5d3b7ec5fe43f875e2c6a549c3a92f31297e
Crghilardi <Crghilardi@yahoo.com>
2017-08-19 18:46:15 -0700

    tinkering with algorithm, still not working, sometimes infinite loop
    I don't know how to structure the firstTime call so that it works
    without overwriting constantly

--- src/FitPointGridInPolygon.py
+++ src/FitPointGridInPolygon.py
@@ -1,3 +1,7 @@
+#this is meant to be run as a script from within QGIS
+#Processing -> Toolbox -> Scripts -> Add script from file
+# this makes a local copy of the script in C:\Users\YourName\.qgis2\processing\scripts
+
 ##input_layer=vector
 #nplots_field=field input_layer
 #output_layer=output vector
@@ -6,18 +10,18 @@ from PyQt4.QtCore import *
 from math import *
 
 #TO DO:
-#I have no idea hwo to handle CRS/units with this...
+#I have no idea how to handle CRS/units with this...?
 
 NPoints=25
-MaxReps=300
+MaxReps=6
 
 #testing point
 pt=QgsPoint(-115.00886,44.15480)
 
 #create empty point layer to add to
-points= QgsVectorLayer("Point?crs=epsg:4326&field=mytext:string(255)&field=mytext2:string(255)", "temporary_points", "memory")
-pr = points.dataProvider()
-pr.addAttributes([ QgsField("X", QVariant.Double), QgsField("Y", QVariant.Double) ]) 
+#points= QgsVectorLayer("Point?crs=epsg:4326&field=mytext:string(255)&field=mytext2:string(255)", "temporary_points", "memory")
+#pr = points.dataProvider()
+#pr.addAttributes([ QgsField("X", QVariant.Double), QgsField("Y", QVariant.Double) ])
 # need to change to float?
 
 #fet.setGeometry(QgsGeometry.fromPoint(bbox1))
@@ -29,59 +33,77 @@ inlayer = processing.getObject(input_layer)
 def print_specs():
     for polygon in inlayer.getFeatures():
         bounds = polygon.geometry().boundingBox() #bounding box object
-        xmin = bounds.xMinimum() 
+        xmin = bounds.xMinimum()
         xmax = bounds.xMaximum()
         ymin = bounds.yMinimum()
         ymax = bounds.yMaximum()
         index = QgsSpatialIndex() #returns object
         area = polygon.geometry().area() #gets passed object
+        dist=sqrt(area / NPoints)
         distInf = sqrt(area / (NPoints + 2))
         distSup =sqrt(area /(NPoints - min(2, NPoints-1)))
         inshape=polygon.geometry().contains(pt)
     print xmax,"\n",ymax,"\n",xmin,"\n",ymin,"\n",index,"\n",area,"\n",distInf,"\n",distSup,"\n",inshape
-print_specs()
+#print_specs()
 
-pointsIn=0
-rep=0
+#output from Idaho only WGS84
+xmax=-111.0467713
+ymax=48.9999503731
+#xmin=-117.236921306
+#ymin=41.9945993666
+#<qgis._core.QgsSpatialIndex object at 0x0000000022C969D8>
+area=24.3915951649
+dist=4.87831903298 #~ approx, did on calculator
+distInf=0.95046957527
+distSup=1.02980781586
+#True
 
-#while PointsIn != Npoints & rep<MaxReps 
-# cpp algo uses a do-while, not sure how to emulate 100% in python
 
-#cpp psuedo code
-# for (x=xmin to xmax by x+dist)
-    # for(y=ymin to ymax by y+dist)
-        # if shape contains (x,y)
-        # shape2=Add_shape()
-        # shape2=Set_value(0,x)
-        # shape2=Set_value(1,y)
-        # PointsIn+=1
-        # rep +=1
+xmin=-116.236921306 #known interior point for testing
+ymin=42.9945993666
 
+
+for polygon in inlayer.getFeatures():
+
+    pointsIn=0
+    rep=0
+
+    x=xmin
+    y=ymin
+    testing=[] #for testing
+    distInf=0.95046957527
+    distSup=1.02980781586
 #python implementation of pseudocode?
-# x=xmin
-# y=ymin
-# if not x >= xmax
-    # if not y >= ymax
-        # if polygon.geometry().contains (x,y)
-            # feat = QgsFeature()
-            # feat.setAttributes([x,y])
-            # pt=QgsPoint(x,y)
-            # fet.setGeometry(QgsGeometry.fromPoint(pt))
-            # pr.addFeatures([fet])
-        # y+=dist
-    # x+=dist
-    #rep+=1
-# continue #? needed?
-
-# #no idea how to nest the tail end of this with above loops
-# if PointsIn > Npoints
-    # distInf = dist
-    # dist = (distInf + distSup)/2
-# elif PointsIn < Npoints
-    # distSup = dist
-    # dist = (distInf + distSup)/2
+    firstTime=True
+    rep+=1
+    while pointsIn != NPoints and rep <= MaxReps:
+        if not firstTime: #if I am reading the Cpp correctly, if firstTime = False, we delete everything and try again?
+            testing=[] #this is overwriting, if you change to testing=testing it contains the stsrting point
+        if not x >= xmax:
+            if not y >= ymax:
+                if polygon.geometry().contains (QgsPoint(x,y)):
+                #feat = QgsFeature()
+                # feat.setAttributes([x,y])
+                # pt=QgsPoint(x,y)
+                # fet.setGeometry(QgsGeometry.fromPoint(pt))
+                # pr.addFeatures([fet])
+                    testing.append([x,y]) # for testing
+                    pointsIn+=1
+        if pointsIn > NPoints:
+            distInf = dist
+            dist = (distInf + distSup)/2
+        elif pointsIn < NPoints:
+            distSup = dist
+            dist = (distInf + distSup)/2
+        #print "y=", y
+            y+=dist
+        #print "x=", x
+            x+=dist
+    firstTime=False
+    # no idea how to nest the tail end of this with above loops
+       
+    print testing
+print "Done!"
 
 #add point layer to QGIS map
-QgsMapLayerRegistry.instance().addMapLayer(points)
-
-#return outPoints
+#QgsMapLayerRegistry.instance().addMapLayer(points)
