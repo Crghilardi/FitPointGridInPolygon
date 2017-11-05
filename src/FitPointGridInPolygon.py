19205ed90d020a9031b83cfdb3b7f807f3b11ac2
Casey Ghilardi <Crghilardi@gmail.com>
2017-11-04 21:20:21 -0700

    update; still not working but now multiple points

--- src/FitPointGridInPolygon.py
+++ src/FitPointGridInPolygon.py
@@ -1,4 +1,4 @@
-#this is meant to be run as a script from within QGIS
+#this is meant to be runs a script from within QGIS
 #Processing -> Toolbox -> Scripts -> Add script from file
 # this makes a local copy of the script in C:\Users\YourName\.qgis2\processing\scripts
 
@@ -12,11 +12,11 @@ from math import *
 #TO DO:
 #I have no idea how to handle CRS/units with this...?
 
-NPoints=25
-MaxReps=350
+#NPoints=25
+#MaxReps=350
 
 #testing point
-pt=QgsPoint(-115.00886,44.15480)
+#pt=QgsPoint(-115.00886,44.15480)
 
 #create empty point layer to add to
 #points= QgsVectorLayer("Point?crs=epsg:4326&field=mytext:string(255)&field=mytext2:string(255)", "temporary_points", "memory")
@@ -43,62 +43,74 @@ def print_specs():
         distInf = sqrt(area / (NPoints + 2))
         distSup =sqrt(area /(NPoints - min(2, NPoints-1)))
         inshape=polygon.geometry().contains(pt)
-    print xmax,"\n",ymax,"\n",xmin,"\n",ymin,"\n",index,"\n",area,"\n",distInf,"\n",distSup,"\n",inshape
+    print "xmax:",xmax,"\n","ymax:",ymax,"\n","xmin:",xmin,"\n","ymin",ymin,"\n","index",index,"\n","area:",area,"\n","distInf:",distInf,"\n","distSup:",distSup,"\n",inshape
+
 #print_specs()
 
 #output from Idaho only WGS84
-xmax=-111.0467713
-ymax=48.9999503731
+#xmax=-111.0467713
+#ymax=48.9999503731
 #xmin=-117.236921306
 #ymin=41.9945993666
 #<qgis._core.QgsSpatialIndex object at 0x0000000022C969D8>
-area=24.3915951649
-dist=4.87831903298 #~ approx, did on calculator
-distInf=0.95046957527
-distSup=1.02980781586
+#area=24.3915951649
+#dist=4.87831903298 #~ approx, did on calculator
+#distInf=0.95046957527
+#distSup=1.02980781586
 #True
 
 
-xmin=-116.236921306 #known interior point for testing
-ymin=42.9945993666
+#xmin=-116.236921306 #known interior point for testing
+#ymin=42.9945993666
 
 #python implementation of pseudocode?
+
+NPoints=25
+MaxReps=350
 for polygon in inlayer.getFeatures():
-    
+    bounds = polygon.geometry().boundingBox()
     rep=0
-    x=xmin
-    y=ymin
+    x=bounds.xMinimum()
+    y=bounds.yMinimum()
     testing=[] #for testing
-    distInf=0.95046957527
-    distSup=1.02980781586
-    pointsIn=0
-    while pointsIn != NPoints and rep < MaxReps:
-        firstTime=True
-        rep+=1
+    area = polygon.geometry().area() 
+    dist=sqrt(area / NPoints)
+    distInf = sqrt(area / (NPoints + 2))
+    distSup =sqrt(area /(NPoints - min(2, NPoints-1)))
+    #dist=4.87831903298 
+    #distInf=0.95046957527
+    #distSup=1.02980781586
+    #pointsIn=0
+    pointsIn=len(testing)
+    firstTime=True
+    while pointsIn != NPoints and rep<MaxReps:
         if not firstTime: #if I am reading the Cpp correctly, if firstTime = False, we delete everything and try again?
-            testing=[] #this is overwriting, if you change to testing=testing it contains the stsrting point
-        if not x >= xmax:
-            if not y >= ymax:
+            testing=[]
+        rep+=1
+        print "rep=",rep
+        if not x >= bounds.xMaximum:
+            if not y >= bounds.yMaximum:
                 if polygon.geometry().contains (QgsPoint(x,y)):
-                #feat = QgsFeature()
-                # feat.setAttributes([x,y])
-                # pt=QgsPoint(x,y)
-                # fet.setGeometry(QgsGeometry.fromPoint(pt))
-                # pr.addFeatures([fet])
+                    #feat = QgsFeature()
+                    # feat.setAttributes([x,y])
+                    # pt=QgsPoint(x,y)
+                    # fet.setGeometry(QgsGeometry.fromPoint(pt))
+                    # pr.addFeatures([fet])
                     testing.append([x,y]) # for testing
                     pointsIn+=1
-                y+=dist
-            x+=dist
+            print "sup,inf, dist is:" , [distSup,distInf,dist]
+            print "y=", y
+            print "x=", x
+            print testing
+            print firstTime
             if pointsIn > NPoints:
                 distInf = dist
                 dist = (distInf + distSup)/2
             elif pointsIn < NPoints:
                 distSup = dist
                 dist = (distInf + distSup)/2
-            print [distSup,distInf,dist]
-            print "y=", y
-            print "x=", x
-            print testing
+            x+=dist
+        y+=dist
     firstTime=False
 print "Done!"
 
